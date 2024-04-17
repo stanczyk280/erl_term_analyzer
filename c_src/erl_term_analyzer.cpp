1f9a8d5763066873b2d0afb5e2d1d2369a7e2fbf
stanczyk280 <bartek.grabowski.280@gmail.com>
2024-04-17 13:49:20 +0200

    +analyzer returns an erlang map instead of printing info

--- c_src/erl_term_analyzer.cpp
+++ c_src/erl_term_analyzer.cpp
@@ -2,24 +2,34 @@
 #include <cstdint>
 #include "term_tags.h"
 
+typedef struct TermInfo
+{
+    char* tag;
+    char* type;
+    uint64_t value;
+    TermInfo* next_term;
+
+}TermInfo;
+
+
 /* NIF interface declarations */
 static int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info);
 static int upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info);
 static void unload(ErlNifEnv* env, void* priv_data);
 
 /* The NIFs: */
-static ERL_NIF_TERM noop(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 static ERL_NIF_TERM echo(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 static ERL_NIF_TERM analyze_term(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
 /* Analyzer methods declarations */
 void print_binary(uint64_t term, uint64_t mask);
-void analyze_immed1(uint64_t term_64);
-void analyze_list(uint64_t term_64);
-void analyze_tag(uint64_t term_64);
-void analyze_immed2(uint64_t term_64);
-void analyze_boxed(uint64_t term_64);
-void analyze_header(uint64_t term_64);
+void print_term_info(TermInfo* info);
+TermInfo* analyze_immed1(uint64_t term_64);
+TermInfo* analyze_list(uint64_t term_64);
+TermInfo* analyze_tag(uint64_t term_64);
+TermInfo* analyze_immed2(uint64_t term_64);
+TermInfo* analyze_boxed(uint64_t term_64);
+TermInfo* analyze_header(uint64_t term_64);
 
 static ErlNifFunc nif_funcs[] = {
     {"echo", 1, echo},
@@ -55,230 +65,295 @@ static int upgrade(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_N
     return 0;
 }
 
-
 static ERL_NIF_TERM echo(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     return argv[0];
+}
+
+static ERL_NIF_TERM term_info_to_map(ErlNifEnv* env, TermInfo* term_info)
+{
+    ERL_NIF_TERM map = enif_make_new_map(env);
+
+    ERL_NIF_TERM key, value;
+
+    key = enif_make_atom(env, "tag");
+    value = enif_make_string(env, term_info->tag, ERL_NIF_LATIN1);
+    enif_make_map_put(env, map, key, value, &map);
+
+    key = enif_make_atom(env, "type");
+    value = enif_make_string(env, term_info->type ? term_info->type : "(null)", ERL_NIF_LATIN1);
+    enif_make_map_put(env, map, key, value, &map);
+
+    key = enif_make_atom(env, "value");
+    value = enif_make_uint64(env, term_info->value);
+    enif_make_map_put(env, map, key, value, &map);
+
+    if (term_info->next_term != NULL) {
+        key = enif_make_atom(env, "next_term");
+        value = term_info_to_map(env, term_info->next_term);
+        enif_make_map_put(env, map, key, value, &map);
+    }
+
+    return map;
 }
 
 static ERL_NIF_TERM analyze_term(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     uint64_t term_64;
     term_64 = uint64_t(argv[0]);
-    analyze_tag(term_64);
-    return atom_ok;
+    TermInfo* term_info = analyze_tag(term_64);
+
+    return term_info_to_map(env, term_info);
 }
 
-void analyze_tag(uint64_t term_64)
+TermInfo* analyze_tag(uint64_t term_64)
 {
+    TermInfo* term_info;
+
     switch(term_64 & _TAG_PRIMARY_MASK)
     {
         case TAG_PRIMARY_HEADER:
-            printf("TAG: HEADER\n");
-            printf("TAG bits: ");
-            print_binary(term_64, _TAG_PRIMARY_MASK);
-            analyze_header(term_64);
+            term_info = analyze_header(term_64);
             break;
         case TAG_PRIMARY_LIST:
-            printf("TAG: LIST\n");
-            printf("TAG bits: ");
-            print_binary(term_64, _TAG_PRIMARY_MASK);
-            analyze_list(term_64);
+            term_info = analyze_list(term_64);
             break;
         case TAG_PRIMARY_BOXED:
-            printf("TAG: BOXED\n");
-            printf("TAG bits: ");
-            print_binary(term_64, _TAG_PRIMARY_MASK);
-            analyze_boxed(term_64);
+            term_info = analyze_boxed(term_64);
             break;
         case TAG_PRIMARY_IMMED1:
-            printf("TAG: IMMED1\n");
-            printf("TAG bits: ");
-            print_binary(term_64, _TAG_PRIMARY_MASK);
-            analyze_immed1(term_64);
+            term_info = analyze_immed1(term_64);
             break;
         default:
             printf("TAG: UNKNOWN\n");
+            term_info = NULL;
             break;
     }
+
+    return term_info;
 }
 
-void analyze_list(uint64_t term_64)
+TermInfo* analyze_list(uint64_t term_64)
 {
+    TermInfo* term_info = (TermInfo*)malloc(sizeof(TermInfo));
+
+    char* tag;
+    tag = "LIST";
+
     uint64_t list_ptr_value;
     list_ptr_value = term_64 & ~ _TAG_PRIMARY_MASK;
-    printf("LIST POINTER: %lu\n", list_ptr_value);
-    printf("LIST POINTER bits: ");
-    print_binary(list_ptr_value, _VALUE_MASK);
-    printf("NEXT TERM: \n");
     uint64_t* list_ptr;
     list_ptr = (uint64_t *)list_ptr_value;
-    analyze_tag(*list_ptr);
+    term_info -> tag = tag;
+    term_info -> type = NULL;
+    term_info -> value = list_ptr_value;
+    term_info -> next_term = analyze_tag(*list_ptr);
+
+    return term_info;
 }
 
-void analyze_boxed(uint64_t term_64) {
+TermInfo* analyze_boxed(uint64_t term_64) 
+{
+    TermInfo* term_info = (TermInfo*)malloc(sizeof(TermInfo));
+
+    char* tag;
+    tag = "BOXED";
+
     uint64_t boxed_ptr_value;
     boxed_ptr_value = term_64 & ~_TAG_PRIMARY_MASK;
-    printf("BOXED POINTER: %d\n", boxed_ptr_value);
-    printf("BOXED POINTER bits: ");
-    print_binary(boxed_ptr_value, _VALUE_MASK);
-    printf("NEXT TERM: \n");
     uint64_t* boxed_ptr;
     boxed_ptr = (uint64_t *)boxed_ptr_value;
-    analyze_tag(*boxed_ptr);
+
+    term_info -> tag = tag;
+    term_info -> type = NULL;
+    term_info -> value = boxed_ptr_value;
+    term_info -> next_term = analyze_tag(*boxed_ptr);
+
+    return term_info;
 }
 
-void analyze_header(uint64_t term_64)
+TermInfo* analyze_header(uint64_t term_64)
 {
     uint64_t header = term_64 & _TAG_HEADER_MASK;
+    TermInfo* term_info = (TermInfo*)malloc(sizeof(TermInfo));
+
+    char* tag;
+    tag = "HEADER";
+
     switch (header)
     {
     case _TAG_HEADER_ARITYVAL:
-        printf("SUBTAG: ARITYVAL\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
-        printf("ARITY VALUE: %lu\n", term_64 >> _HEADER_ARITY_OFFS);
-        printf("ARITY VALUE bits after bsl of 6: ");
-        print_binary(term_64 >> _HEADER_ARITY_OFFS, _VALUE_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "ARITYVAL";
+        term_info -> value = term_64 >> _HEADER_ARITY_OFFS;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_FUN_REF:
-        printf("SUBTAG: FUN_REF\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "FUN_REF";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_FUN:
-        printf("SUBTAG: FUN\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "FUN";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_POS_BIG:
-        printf("SUBTAG: POS_BIG\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "POS_BIG";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_NEG_BIG:
-        printf("SUBTAG: NEG_BIG\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "NEG_BIG";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_FLOAT:
-        printf("SUBTAG: FLOAT\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "FLOAT";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_REF:
-        printf("SUBTAG: REF\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
-        printf("ARITY VALUE: %lu\n", term_64 >> _HEADER_ARITY_OFFS);
-        printf("ARITY VALUE bits after bsl of 6: ");
-        print_binary(term_64 >> _HEADER_ARITY_OFFS, _VALUE_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "REF";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_BIN_REF:
-        printf("SUBTAG: BIN_REF\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "BIN_REF";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_HEAP_BITS:
-        printf("SUBTAG: HEAP_BITS\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "HEAP_BITS";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_SUB_BITS:
-        printf("SUBTAG: SUB_BITS\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "SUB_BITS";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_EXTERNAL_PID:
-        printf("SUBTAG: EXTERNAL_PID\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "EXTERNAL_PID";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_EXTERNAL_PORT: 
-        printf("SUBTAG: EXTERNAL_PORT\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "EXTERNAL_PORT";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_EXTERNAL_REF:
-        printf("SUBTAG: EXTERNAL_REF\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "EXTERNAL_REF";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     case _TAG_HEADER_MAP:
-        printf("SUBTAG: MAP\n");
-        printf("SUBTAG bits: ");
-        print_binary(term_64, _HEADER_SUBTAG_MASK);
+        term_info -> tag = tag;
+        term_info -> type = "MAP";
+        term_info -> value = NULL;
+        term_info -> next_term = NULL;
         break;
     }
+
+    return term_info;
 }
 
-void analyze_immed1(uint64_t term_64)
+TermInfo* analyze_immed1(uint64_t term_64)
 {
     uint64_t immed1_term = term_64 & _TAG_IMMED1_MASK;
     uint64_t pid, port, small_int;
+
+    char* tag;
+
+    TermInfo* term_info = (TermInfo*)malloc(sizeof(TermInfo));
+
+    tag = "IMMED1";
+
     switch(immed1_term)
     {
         case _TAG_IMMED1_PID:
-            printf("TYPE: PID\n");
             pid = term_64 >> _TAG_IMMED1_SIZE;
-            printf("TYPE bits: ");
-            print_binary(term_64, _TYPE_IMMED1_MASK);
-            printf("VALUE: %lu\n", pid);
+            term_info -> tag = tag;
+            term_info -> type = "PID";
+            term_info -> value = pid;
+            term_info -> next_term = NULL;
             break;
         case _TAG_IMMED1_PORT:
-            printf("TYPE: PORT\n");
             port = term_64 >> _TAG_IMMED1_SIZE;
-            printf("TYPE bits: ");
-            print_binary(term_64, _TYPE_IMMED1_MASK);
-            printf("VALUE: %lu\n", port);
+            term_info -> tag = tag;
+            term_info -> type = "PORT";
+            term_info -> value = port;
+            term_info -> next_term = NULL;
             break;
         case _TAG_IMMED1_IMMED2:
-            printf("SUBTAG: IMMED2\n");
-            printf("SUBTAG bits: ");
-            print_binary(term_64, _TYPE_IMMED1_MASK);
-            analyze_immed2(term_64);
+            term_info -> tag = tag;
+            term_info -> type = "IMMED2";
+            term_info -> value = NULL;
+            term_info -> next_term = analyze_immed2(term_64);
             break;
         case _TAG_IMMED1_SMALL:
-            printf("TYPE: SMALL INT\n");
             small_int = term_64 >> _TAG_IMMED1_SIZE;
-            printf("TYPE bits: ");
-            print_binary(term_64, _TYPE_IMMED1_MASK);
-            printf("VALUE: %lu\n", small_int);
-            printf("VALUE bits after bsl of 4: ");
-            print_binary(small_int, _VALUE_MASK);
+            term_info -> tag = tag;
+            term_info -> type = "SMALL INT";
+            term_info -> value = small_int;
+            term_info -> next_term = NULL;
             break;
         default:
             printf("IMMED1: UNKNOWN\n");
             break;
     
     }
+
+    return term_info;
 }
 
-void analyze_immed2(uint64_t term_64)
+TermInfo* analyze_immed2(uint64_t term_64)
 {
     uint64_t immed2_term;
     immed2_term = term_64 & _TAG_IMMED2_MASK;
+
+    TermInfo* term_info = (TermInfo*)malloc(sizeof(TermInfo));
+    char* tag;
+    tag = "IMMED2";
     switch (immed2_term)
     {
         case _TAG_IMMED2_ATOM:
-            printf("TYPE: ATOM\n");
-            printf("TYPE bits: ");
-            print_binary(term_64, _TYPE_IMMED2_MASK);
+            term_info -> tag = tag;
+            term_info -> type = "ATOM";
+            term_info -> value = NULL;
+            term_info -> next_term = NULL;
             break;
         case _TAG_IMMED2_CATCH:
-            printf("TYPE: CATCH\n");
-            printf("TYPE bits: ");
-            print_binary(term_64, _TYPE_IMMED2_MASK);
+            term_info -> tag = tag;
+            term_info -> type = "CATCH";
+            term_info -> value = NULL;
+            term_info -> next_term = NULL;
             break;
         case _TAG_IMMED2_NIL:
-            printf("TYPE: NIL\n");
-            printf("TYPE bits: ");
-            print_binary(term_64, _TYPE_IMMED2_MASK);
+            term_info -> tag = tag;
+            term_info -> type = "NILL";
+            term_info -> value = NULL;
+            term_info -> next_term = NULL;
             break;
         default:
             printf("TYPE: UNKNOWN\n");
             break;
     }
+
+    return term_info;
 }
 
 void print_binary(uint64_t num, uint64_t mask) {
@@ -302,4 +377,13 @@ void print_binary(uint64_t num, uint64_t mask) {
         printf("]");
     }
     printf("\n");
+}
+
+void print_term_info(TermInfo* info) {
+    printf("TAG: %s\n", info->tag);
+    printf("TYPE: %s\n", info->type);
+    printf("VALUE: %lu\n", info->value);
+    if (info->next_term != NULL) {
+        print_term_info(info->next_term);
+    }
 }
