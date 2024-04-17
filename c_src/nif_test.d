46a2cd441b287307965030b0da8c5c5d3d0b704d
stanczyk280 <bartek.grabowski.280@gmail.com>
2024-04-17 10:42:04 +0200

    code cleanup

--- c_src/nif_test.d
+++
@@ -1,6 +0,0 @@
-c_src/nif_test.o: c_src/nif_test.cpp \
- /home/bgrabowski/.evm/erlang_versions/otp_src_26.2.2/lib/erlang/erts-14.2.2/include/erl_nif.h \
- /home/bgrabowski/.evm/erlang_versions/otp_src_26.2.2/lib/erlang/erts-14.2.2/include/erl_drv_nif.h \
- /home/bgrabowski/.evm/erlang_versions/otp_src_26.2.2/lib/erlang/erts-14.2.2/include/erl_int_sizes_config.h \
- /home/bgrabowski/.evm/erlang_versions/otp_src_26.2.2/lib/erlang/erts-14.2.2/include/erl_nif_api_funcs.h \
- c_src/term_tags.h
